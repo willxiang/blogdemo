eca08f33134cc4b0314de3e88bf5784c2d86e7e1
向炜 <willxiang@qq.com>
2015-10-30 17:29:50 +0800

    add some code

--- app/scripts/controllers/component.js
+++ app/scripts/controllers/component.js
@@ -20,25 +20,20 @@ angular.module('willxiangApp').controller('ComponentCtrl',
 
 
         /** Alert提示框代码 **/
-        $scope.alerts = [{
-            type: 'danger',
-            msg: 'Oh snap! Change a few things up and try submitting again.'
-        }, {
-            type: 'success',
-            msg: 'Well done! You successfully read this important alert message.'
-        }];
+        $scope.alerts = [];
 
-        var alertCount = 1;
+        $scope.alertCount = $scope.alerts.length;
         $scope.addAlert = function() {
             $scope.alerts.push({
                 type: 'info',
-                msg: 'Another alert!+' + alertCount
+                msg: 'Another alert!+' + $scope.alertCount
             });
-            alertCount++;
+            $scope.alertCount++;
         };
 
         $scope.closeAlert = function(index) {
             $scope.alerts.splice(index, 1);
+            $scope.alertCount--;
         };
         /** Alert提示框代码 **/
 
@@ -48,27 +43,19 @@ angular.module('willxiangApp').controller('ComponentCtrl',
 );
 
 /*分页*/
-function pagination(data,pageSize,currentPage,totalCount)
-{
-    var start = (currentPage-1)*pageSize;
+function pagination(data, pageSize, currentPage, totalCount) {
+    var start = (currentPage - 1) * pageSize;
     var newData = [];
-    if(start+pageSize<=totalCount)
-    {
-        for (var i = start; i < start+pageSize; i++) {
+    if (start + pageSize <= totalCount) {
+        for (var i = start; i < start + pageSize; i++) {
             newData.push(data[i]);
         };
-    }
-    else
-    {
-        if(currentPage===1)
-        {
-            for (var i = 0; i < totalCount; i++)
-            {
+    } else {
+        if (currentPage === 1) {
+            for (var i = 0; i < totalCount; i++) {
                 newData.push(data[i]);
             }
-        }
-        else
-        {
+        } else {
             for (var i = start; i < totalCount; i++) {
                 newData.push(data[i]);
             };
@@ -82,16 +69,266 @@ function pagination(data,pageSize,currentPage,totalCount)
 angular.module('willxiangApp').controller('ModalTableCtrl', function($scope, $uibModal) {
 
     //数据源
-     var dataList  = [{        Id: 1,        FirstName: "Mark1",        LastName: "Otto1",        UserName: "@mdo1"    }, {        Id: 2,        FirstName: "Mark2",        LastName: "Otto2",        UserName: "@mdo2"    }, {        Id: 3,        FirstName: "Mark3",        LastName: "Otto3",        UserName: "@mdo3"    }, {        Id: 4,        FirstName: "Mark4",        LastName: "Otto4",        UserName: "@mdo4"    }, {        Id: 5,        FirstName: "Mark5",        LastName: "Otto5",        UserName: "@mdo5"    }, {        Id: 6,        FirstName: "Mark6",        LastName: "Otto6",        UserName: "@mdo6"    }, {        Id: 7,        FirstName: "Mark7",        LastName: "Otto7",        UserName: "@mdo7"    }, {        Id: 8,        FirstName: "Mark8",        LastName: "Otto8",        UserName: "@mdo8"    }, {        Id: 9,        FirstName: "Mark9",        LastName: "Otto9",        UserName: "@mdo9"    }, {        Id: 10,        FirstName: "Mark10",        LastName: "Otto10",        UserName: "@mdo10"    }, {        Id: 11,        FirstName: "Mark11",        LastName: "Otto11",        UserName: "@mdo11"    }, {        Id: 12,        FirstName: "Mark12",        LastName: "Otto12",        UserName: "@mdo12"    }, {        Id: 13,        FirstName: "Mark13",        LastName: "Otto13",        UserName: "@mdo13"    }, {        Id: 14,        FirstName: "Mark14",        LastName: "Otto14",        UserName: "@mdo14"    }, {        Id: 15,        FirstName: "Mark15",        LastName: "Otto15",        UserName: "@mdo15"    }, {        Id: 16,        FirstName: "Mark16",        LastName: "Otto16",        UserName: "@mdo16"    }, {        Id: 17,        FirstName: "Mark17",        LastName: "Otto17",        UserName: "@mdo17"    }, {        Id: 18,        FirstName: "Mark18",        LastName: "Otto18",        UserName: "@mdo18"    }, {        Id: 19,        FirstName: "Mark19",        LastName: "Otto19",        UserName: "@mdo19"    }, {        Id: 20,        FirstName: "Mark20",        LastName: "Otto20",        UserName: "@mdo20"    }, {        Id: 21,        FirstName: "Mark21",        LastName: "Otto21",        UserName: "@mdo21"    }, {        Id: 22,        FirstName: "Mark22",        LastName: "Otto22",        UserName: "@mdo22"    }, {        Id: 23,        FirstName: "Mark23",        LastName: "Otto23",        UserName: "@mdo23"    }, {        Id: 24,        FirstName: "Mark24",        LastName: "Otto24",        UserName: "@mdo24"    }, {        Id: 25,        FirstName: "Mark25",        LastName: "Otto25",        UserName: "@mdo25"    }, {        Id: 26,        FirstName: "Mark26",        LastName: "Otto26",        UserName: "@mdo26"    }, {        Id: 27,        FirstName: "Mark27",        LastName: "Otto27",        UserName: "@mdo27"    }, {        Id: 28,        FirstName: "Mark28",        LastName: "Otto28",        UserName: "@mdo28"    }, {        Id: 29,        FirstName: "Mark29",        LastName: "Otto29",        UserName: "@mdo29"    }, {        Id: 30,        FirstName: "Mark30",        LastName: "Otto30",        UserName: "@mdo30"    }, {        Id: 31,        FirstName: "Mark31",        LastName: "Otto31",        UserName: "@mdo31"    }, {        Id: 32,        FirstName: "Mark32",        LastName: "Otto32",        UserName: "@mdo32"    }, {        Id: 33,        FirstName: "Mark33",        LastName: "Otto33",        UserName: "@mdo33"    }, {        Id: 34,        FirstName: "Mark34",        LastName: "Otto34",        UserName: "@mdo34"    }, {        Id: 35,        FirstName: "Mark35",        LastName: "Otto35",        UserName: "@mdo35"    }, {        Id: 36,        FirstName: "Mark36",        LastName: "Otto36",        UserName: "@mdo36"    }, {        Id: 37,        FirstName: "Mark37",        LastName: "Otto37",        UserName: "@mdo37"    }, {        Id: 38,        FirstName: "Mark38",        LastName: "Otto38",        UserName: "@mdo38"    }, {        Id: 39,        FirstName: "Mark39",        LastName: "Otto39",        UserName: "@mdo39"    }, {        Id: 40,        FirstName: "Mark40",        LastName: "Otto40",        UserName: "@mdo40"    }, {        Id: 41,        FirstName: "Mark41",        LastName: "Otto41",        UserName: "@mdo41"    }, {        Id: 42,        FirstName: "Mark42",        LastName: "Otto42",        UserName: "@mdo42"    }, {        Id: 43,        FirstName: "Mark43",        LastName: "Otto43",        UserName: "@mdo43"    }, {        Id: 44,        FirstName: "Mark44",        LastName: "Otto44",        UserName: "@mdo44"    }, {        Id: 45,        FirstName: "Mark45",        LastName: "Otto45",        UserName: "@mdo45"    }, {        Id: 46,        FirstName: "Mark46",        LastName: "Otto46",        UserName: "@mdo46"    }, {        Id: 47,        FirstName: "Mark47",        LastName: "Otto47",        UserName: "@mdo47"    }, {        Id: 48,        FirstName: "Mark48",        LastName: "Otto48",        UserName: "@mdo48"    }, {        Id: 49,        FirstName: "Mark49",        LastName: "Otto49",        UserName: "@mdo49"    }, {        Id: 50,        FirstName: "Mark50",        LastName: "Otto50",        UserName: "@mdo50"    }];
+    var dataList = [{
+        Id: 1,
+        FirstName: "Mark1",
+        LastName: "Otto1",
+        UserName: "@mdo1"
+    }, {
+        Id: 2,
+        FirstName: "Mark2",
+        LastName: "Otto2",
+        UserName: "@mdo2"
+    }, {
+        Id: 3,
+        FirstName: "Mark3",
+        LastName: "Otto3",
+        UserName: "@mdo3"
+    }, {
+        Id: 4,
+        FirstName: "Mark4",
+        LastName: "Otto4",
+        UserName: "@mdo4"
+    }, {
+        Id: 5,
+        FirstName: "Mark5",
+        LastName: "Otto5",
+        UserName: "@mdo5"
+    }, {
+        Id: 6,
+        FirstName: "Mark6",
+        LastName: "Otto6",
+        UserName: "@mdo6"
+    }, {
+        Id: 7,
+        FirstName: "Mark7",
+        LastName: "Otto7",
+        UserName: "@mdo7"
+    }, {
+        Id: 8,
+        FirstName: "Mark8",
+        LastName: "Otto8",
+        UserName: "@mdo8"
+    }, {
+        Id: 9,
+        FirstName: "Mark9",
+        LastName: "Otto9",
+        UserName: "@mdo9"
+    }, {
+        Id: 10,
+        FirstName: "Mark10",
+        LastName: "Otto10",
+        UserName: "@mdo10"
+    }, {
+        Id: 11,
+        FirstName: "Mark11",
+        LastName: "Otto11",
+        UserName: "@mdo11"
+    }, {
+        Id: 12,
+        FirstName: "Mark12",
+        LastName: "Otto12",
+        UserName: "@mdo12"
+    }, {
+        Id: 13,
+        FirstName: "Mark13",
+        LastName: "Otto13",
+        UserName: "@mdo13"
+    }, {
+        Id: 14,
+        FirstName: "Mark14",
+        LastName: "Otto14",
+        UserName: "@mdo14"
+    }, {
+        Id: 15,
+        FirstName: "Mark15",
+        LastName: "Otto15",
+        UserName: "@mdo15"
+    }, {
+        Id: 16,
+        FirstName: "Mark16",
+        LastName: "Otto16",
+        UserName: "@mdo16"
+    }, {
+        Id: 17,
+        FirstName: "Mark17",
+        LastName: "Otto17",
+        UserName: "@mdo17"
+    }, {
+        Id: 18,
+        FirstName: "Mark18",
+        LastName: "Otto18",
+        UserName: "@mdo18"
+    }, {
+        Id: 19,
+        FirstName: "Mark19",
+        LastName: "Otto19",
+        UserName: "@mdo19"
+    }, {
+        Id: 20,
+        FirstName: "Mark20",
+        LastName: "Otto20",
+        UserName: "@mdo20"
+    }, {
+        Id: 21,
+        FirstName: "Mark21",
+        LastName: "Otto21",
+        UserName: "@mdo21"
+    }, {
+        Id: 22,
+        FirstName: "Mark22",
+        LastName: "Otto22",
+        UserName: "@mdo22"
+    }, {
+        Id: 23,
+        FirstName: "Mark23",
+        LastName: "Otto23",
+        UserName: "@mdo23"
+    }, {
+        Id: 24,
+        FirstName: "Mark24",
+        LastName: "Otto24",
+        UserName: "@mdo24"
+    }, {
+        Id: 25,
+        FirstName: "Mark25",
+        LastName: "Otto25",
+        UserName: "@mdo25"
+    }, {
+        Id: 26,
+        FirstName: "Mark26",
+        LastName: "Otto26",
+        UserName: "@mdo26"
+    }, {
+        Id: 27,
+        FirstName: "Mark27",
+        LastName: "Otto27",
+        UserName: "@mdo27"
+    }, {
+        Id: 28,
+        FirstName: "Mark28",
+        LastName: "Otto28",
+        UserName: "@mdo28"
+    }, {
+        Id: 29,
+        FirstName: "Mark29",
+        LastName: "Otto29",
+        UserName: "@mdo29"
+    }, {
+        Id: 30,
+        FirstName: "Mark30",
+        LastName: "Otto30",
+        UserName: "@mdo30"
+    }, {
+        Id: 31,
+        FirstName: "Mark31",
+        LastName: "Otto31",
+        UserName: "@mdo31"
+    }, {
+        Id: 32,
+        FirstName: "Mark32",
+        LastName: "Otto32",
+        UserName: "@mdo32"
+    }, {
+        Id: 33,
+        FirstName: "Mark33",
+        LastName: "Otto33",
+        UserName: "@mdo33"
+    }, {
+        Id: 34,
+        FirstName: "Mark34",
+        LastName: "Otto34",
+        UserName: "@mdo34"
+    }, {
+        Id: 35,
+        FirstName: "Mark35",
+        LastName: "Otto35",
+        UserName: "@mdo35"
+    }, {
+        Id: 36,
+        FirstName: "Mark36",
+        LastName: "Otto36",
+        UserName: "@mdo36"
+    }, {
+        Id: 37,
+        FirstName: "Mark37",
+        LastName: "Otto37",
+        UserName: "@mdo37"
+    }, {
+        Id: 38,
+        FirstName: "Mark38",
+        LastName: "Otto38",
+        UserName: "@mdo38"
+    }, {
+        Id: 39,
+        FirstName: "Mark39",
+        LastName: "Otto39",
+        UserName: "@mdo39"
+    }, {
+        Id: 40,
+        FirstName: "Mark40",
+        LastName: "Otto40",
+        UserName: "@mdo40"
+    }, {
+        Id: 41,
+        FirstName: "Mark41",
+        LastName: "Otto41",
+        UserName: "@mdo41"
+    }, {
+        Id: 42,
+        FirstName: "Mark42",
+        LastName: "Otto42",
+        UserName: "@mdo42"
+    }, {
+        Id: 43,
+        FirstName: "Mark43",
+        LastName: "Otto43",
+        UserName: "@mdo43"
+    }, {
+        Id: 44,
+        FirstName: "Mark44",
+        LastName: "Otto44",
+        UserName: "@mdo44"
+    }, {
+        Id: 45,
+        FirstName: "Mark45",
+        LastName: "Otto45",
+        UserName: "@mdo45"
+    }, {
+        Id: 46,
+        FirstName: "Mark46",
+        LastName: "Otto46",
+        UserName: "@mdo46"
+    }, {
+        Id: 47,
+        FirstName: "Mark47",
+        LastName: "Otto47",
+        UserName: "@mdo47"
+    }, {
+        Id: 48,
+        FirstName: "Mark48",
+        LastName: "Otto48",
+        UserName: "@mdo48"
+    }, {
+        Id: 49,
+        FirstName: "Mark49",
+        LastName: "Otto49",
+        UserName: "@mdo49"
+    }, {
+        Id: 50,
+        FirstName: "Mark50",
+        LastName: "Otto50",
+        UserName: "@mdo50"
+    }];
 
 
     /*分页相关代码*/
     $scope.currentPage = 1;
-    $scope.pageSize =5;
+    $scope.pageSize = 5;
     $scope.totalCount = dataList.length;
-    $scope.dataSource = pagination(dataList,$scope.pageSize,$scope.currentPage,$scope.totalCount);
-    $scope.pageChanged = function(){
-        $scope.dataSource = pagination(dataList,$scope.pageSize,$scope.currentPage,$scope.totalCount);
+    $scope.dataSource = pagination(dataList, $scope.pageSize, $scope.currentPage, $scope.totalCount);
+    $scope.pageChanged = function() {
+        $scope.dataSource = pagination(dataList, $scope.pageSize, $scope.currentPage, $scope.totalCount);
     }
 
     /*分页相关代码*/
@@ -149,4 +386,75 @@ angular.module('willxiangApp').controller('ModalInstanceCtrl', function($scope, 
     $scope.cancel = function() {
         $uibModalInstance.dismiss('cancel');
     };
+});
+
+
+
+
+angular.module('willxiangApp').controller('TypeaheadCtrl', function($scope, $http) {
+    $scope.selected = undefined;
+    // Any function returning a promise object can be used to load values asynchronously
+    $scope.getLocation = function(val) {
+        return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+                address: val,
+                sensor: false
+            }
+        }).then(function(response) {
+            return response.data.results.map(function(item) {
+                console.log(response.data.results);
+                return item.formatted_address;
+            });
+        });
+    };
+
+
+    $scope.getWeather = function(val) {
+        return $http.get('//api.heweather.com/x3/weather', {
+            params: {
+                city: val,
+                key: "6a7d5f6938da427fbb17219e1461e4e4"
+            }
+        }).then(function(response) {
+            if (response.status === 200) {
+                var weather = response.data["HeWeather data service 3.0"]
+                if (weather[0].status === "ok") {
+                    var city = weather[0].basic.city;
+                    var cnty = weather[0].basic.cnty;
+                    var time = weather[0].basic.update.loc;
+                    var cond = weather[0].daily_forecast[0].cond.txt_d;
+                    var min = weather[0].daily_forecast[0].tmp.min;
+                    var max = weather[0].daily_forecast[0].tmp.max;
+                    var wind = weather[0].daily_forecast[0].wind.sc;
+                    return [cnty, city, cond, min + "℃~" + max + "℃", wind, time];
+                } else {
+                    return [weather[0].status + ""];
+                }
+            } else {
+                return ["Not Found"];
+            }
+        });
+
+    };
+
+
+
+
+});
+
+
+//日期选择
+angular.module('willxiangApp').controller('DataPickerCtrl', function($scope) {
+    $scope.formats = ['yyyy-MM-dd','dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+
+    $scope.status = {
+        opened: false
+    };
+    $scope.open = function($event) {
+        $scope.status.opened = true;
+    };
+
+
+
 });
